d4bfc8183f0bd375b1f7e267c0285ddd22cb4977
Tri Ho <vinhocent@gmail.com>
2021-01-10 07:02:08 -0500

    chat message box

--- public/js/client-chat.js
+++ public/js/client-chat.js
@@ -6,56 +6,67 @@ var userid = getUserId();
 var username = getUserName();
 
 function getUserId() {
-	var cookies = document.cookie;
-	var state = false;
-	cookies = cookies.split(";");
-	for (var i = 0; i < cookies.length; i++) {
-		var key = cookies[i].split("=")[0];
-		var sotr = cookies[i].split("=")[1];
-		if (key.localeCompare(" userid") == 0) {
-			return sotr;
-		}
-	}
-	return false;
+  var cookies = document.cookie;
+  var state = false;
+  cookies = cookies.split(";");
+  for (var i = 0; i < cookies.length; i++) {
+    var key = cookies[i].split("=")[0];
+    var sotr = cookies[i].split("=")[1];
+    if (key.localeCompare(" userid") == 0) {
+      return sotr;
+    }
+  }
+  return false;
 }
 
 function getUserName() {
-	var cookies = document.cookie;
-	var state = false;
-	cookies = cookies.split(";");
-	for (var i = 0; i < cookies.length; i++) {
-		var key = cookies[i].split("=")[0];
-		var sotr = cookies[i].split("=")[1];
-		if (key.localeCompare(" username") == 0) {
-			return sotr;
-		}
-	}
-	return false;
+  var cookies = document.cookie;
+  var state = false;
+  cookies = cookies.split(";");
+  for (var i = 0; i < cookies.length; i++) {
+    var key = cookies[i].split("=")[0];
+    var sotr = cookies[i].split("=")[1];
+    if (key.localeCompare(" username") == 0) {
+      return sotr;
+    }
+  }
+  return false;
 }
 
 var form = document.getElementById("form");
 var input = document.getElementById("input");
 
-socket.on("connect", function() {
-	console.log(socket.id);
-	socket.emit("joinRoom", socket.id, username);
+socket.on("connect", function () {
+  console.log(socket.id);
+  socket.emit("joinRoom", socket.id, username);
 });
 
-socket.on("matchFound", function() {
-	document.getElementById("waiting-msg").innerHTML = "Start chatting!";
+socket.on("matchFound", function () {
+  document.getElementById("waiting-msg").innerHTML = "Start chatting!";
 });
 
-form.addEventListener("submit", function(e) {
-	e.preventDefault();
-	if (input.value) {
-		socket.emit("chat message", input.value, username, userid, day);
-		input.value = "";
-	}
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit("chat message", input.value, username, userid, day);
+    input.value = "";
+  }
 });
 
-socket.on("chat message", function(msg) {
-	var item = document.createElement("li");
-	item.textContent = msg;
-	messages.appendChild(item);
-	window.scrollTo(0, document.body.scrollHeight);
+socket.on("chat message", function (msg) {
+  var item = document.createElement("li");
+  item.classList.add("mine");
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+$(function () {
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+    var message = $(".message").first().clone();
+    message.find("p").text($("input").val());
+    message.prependTo(".messagebox");
+    $("input").val("");
+  });
 });
